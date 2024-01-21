bfb82cdee0ec8df6145fcb9e9c14a49d2d495935
pramudithasadeepa <mlpramuditha1@gmail.com>
2024-01-21 14:35:16 +0530

    add another file

---
+++ main.js
@@ -0,0 +1,32 @@
+// console.log("practise 1");
+// console.log("practise 2");
+
+// var value =10;
+// console.log(value);
+
+// var value =20;
+// console.log(value);
+
+// const first_function = () => {
+//     console.log("I am a function");
+// };
+
+
+// let age = prompt("enter your age");
+
+// const abc = (yourname,age) =>{
+//     console.log("your name is "+yourname+" and age is "+age);
+// }
+
+// let a = "123";
+// a = Number(a);
+// console.log(a+1);
+
+function printvalue(price,qty){
+    return ( price * qty );
+    
+}
+
+var value = printvalue(50,2);
+alert(value);
+
